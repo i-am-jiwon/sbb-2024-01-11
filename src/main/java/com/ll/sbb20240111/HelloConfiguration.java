cb53a59746769610b09f58fa27b3dbc154c74f61
i-am-jiwon <dlark1128@gmail.com>
2024-01-11 13:52:31 +0900

    hello

---
+++ src/main/java/com/ll/sbb20240111/HelloConfiguration.java
@@ -0,0 +1,37 @@
+package com.ll.sbb20240111;
+
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.configuration.annotation.JobScope;
+import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.PlatformTransactionManager;
+
+@Configuration
+public class HelloConfiguration {
+    @Bean
+    public Job job(JobRepository jobRepository, Step step1) {
+        return new JobBuilder("job", jobRepository)
+                .start(step1)
+                .build();
+    }
+    @Bean
+    public Step step1(JobRepository jobRepository, Tasklet testTasklet, PlatformTransactionManager platformTransactionManager){
+        return new StepBuilder("step1", jobRepository)
+                .tasklet(testTasklet, platformTransactionManager).build();
+    }
+    @Bean
+    public Tasklet testTasklet(){
+        return ((contribution, chunkContext) -> {
+            System.out.println("Hello World");
+            return RepeatStatus.FINISHED;
+        });
+    }
+}
